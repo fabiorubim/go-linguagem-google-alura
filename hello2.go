8d121797f05e5fd747801cbdcfb06100ea4af84b
Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2022-06-10 00:48:17 -0300

    Utilizando requisições e for indefinidamente com Go

--- hello2.go
+++ hello2.go
@@ -2,27 +2,31 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
-	exibeIntroducao()
+	//Somente o for roda indefinidamente
+	for {
+		exibeIntroducao()
 
-	exibeMenu()
+		exibeMenu()
 
-	comando := leComando()
+		comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo Logs")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando!")
-		os.Exit(-1) //Indica ao S.O que ocorreu algo inesperado
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando!")
+			os.Exit(-1) //Indica ao S.O que ocorreu algo inesperado
+		}
 	}
 }
 
@@ -45,4 +49,17 @@ func exibeMenu() {
 	fmt.Println("1- Iniciar monitoramento")
 	fmt.Println("2- Exibir os Logs")
 	fmt.Println("0- Sair do programa")
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://www.alura.com.br"
+	resp, _ := http.Get(site) //Underline diz que não interessa aquele parâmetros
+	//fmt.Println(resp)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status code:", resp.StatusCode)
+	}
 }
