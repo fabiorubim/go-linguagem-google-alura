7591225d60f2e5224669b362cab81fb19232da2d
Fabio Biasi Mello Rubim <fabiorubim@hotmail.com>
2022-06-11 00:46:21 -0300

    Utilizando array, slices, sleep, constantes, for no Go

---
+++ MonitoringApp.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+const monitoramentos = 3
+const delay = 5
+
+func main() {
+	//Somente o for roda indefinidamente
+
+	for {
+		exibeIntroducao()
+
+		exibeMenu()
+
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando!")
+			os.Exit(-1) //Indica ao S.O que ocorreu algo inesperado
+		}
+	}
+}
+
+func exibeIntroducao() {
+	var nome string = "Fabio"
+	var versao float32 = 1.1
+
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func leComando() int {
+	var comandoLindo int
+	fmt.Scan(&comandoLindo)
+	fmt.Println("O comando escolhido foi:", comandoLindo)
+	fmt.Println("")
+	return comandoLindo
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2- Exibir os Logs")
+	fmt.Println("0- Sair do programa")
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	//O código abaixo foi trocado por Slices
+	//var sites [4]string //Array possuem tamanho fixo em Go
+	// sites[0] = "hrandom-status-code.herokuapp.com"
+	// sites[1] = "www.alura.com.br"
+	// sites[2] = "www.caelum.com.br"
+	// sites[3] = ""
+
+	sites := []string{"http://random-status-code.herokuapp.com", "https://www.alura.com.br", "https://www.caelum.com.br"}
+
+	// fmt.Println(sites)
+
+	//Um tipod e for
+	// for i := 0; i < len(sites); i++ {
+	// 	fmt.Println(sites[i])
+	// }
+
+	for i := 0; i < monitoramentos; i++ {
+		for indice, site := range sites {
+			fmt.Println("Testando site", indice, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+
+}
+
+func testaSite(site string) {
+	resp, _ := http.Get(site) //Underline diz que não interessa aquele parâmetros
+	//fmt.Println(resp)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status code:", resp.StatusCode)
+	}
+}
